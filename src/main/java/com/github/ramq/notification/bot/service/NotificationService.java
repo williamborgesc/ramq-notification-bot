eb37da1e539d9e214eaa6dd788376cf2178cdb7f
William Correia <williamborgesc@gmail.com>
2022-09-29 21:09:32 -0400

    Removing email notification

--- src/main/java/com/github/ramq/notification/bot/service/NotificationService.java
+++ src/main/java/com/github/ramq/notification/bot/service/NotificationService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NotificationService {
 
-    private final EmailService emailService;
-
     private final TwitterService twitterService;
 
     @Value("${application.notification.tweet-message}")
@@ -18,12 +16,9 @@ public class NotificationService {
 
     public void sendNotification() {
         try {
-            emailService.sendSimpleMail();
             twitterService.createTweet(tweetMessage);
         } catch (Exception ex) {
             throw new RuntimeException("Something went wrong when trying to notify:", ex);
         }
     }
-
-
 }
